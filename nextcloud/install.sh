900e04ac00ec83030dc42050e238202a54feabfb
Anton Rychenkov <anton.rychenkov@onlyoffice.com>
2021-10-22 13:33:17 +0400

    refac111

--- nextcloud/install.sh
+++ nextcloud/install.sh
@@ -3,21 +3,13 @@ SERVICE_TAG="latest"
 CONNECTOR_URL="https://github.com/ONLYOFFICE/onlyoffice-jira"
 CONNECTOR_NAME="onlyoffice-integration-web-jira.jar"
 
-install_dependencies() {
+install_nextcloud() {
   source /app/common/install_dependencies.sh
   install_dependencies
-}
-
-get_ip() {
-export EXT_IP=`wget -q -O - ifconfig.me/ip`
-}
-
-install_nextcloud() {
-cd /app/nextcloud/
-docker-compose up -d
+  cd /app/nextcloud/
+  export EXT_IP=`wget -q -O - ifconfig.me/ip`
+  docker-compose up -d
 }
 
 #main
-get_ip
-install_dependencies
 install_nextcloud
