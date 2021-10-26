103a520c907424173101c71ca4e1520bed61b877
Sergey Malkin <sergey.malkin@onlyoffice.com>
2021-10-26 11:26:18 +0300

    Add install file

---
+++ plone/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+install_plone(){
+  source /app/common/install_dependencies.sh
+}
+
+complete_installation(){
+  echo -e "\e[0;32m The script is finished \e[0m"
+}
+
+install_plone
+complete_installation
