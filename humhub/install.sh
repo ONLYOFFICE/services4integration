d03e52a4559b8943095342617f8663dce6c262ef
AlexandrKharitonov <75480173+AleksandrKharitonov@users.noreply.github.com>
2021-10-29 11:25:02 +0300

    Add installation of a humhub stand

---
+++ humhub/install.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+SERVICE_TAG="stable"
+CONNECTOR_URL="https://github.com/ONLYOFFICE/onlyoffice-humhub/releases/download/v2.2.2/onlyoffice.zip"
+CONNECTOR_NAME="onlyoffice.zip"
+source /app/common/check_parameters.sh ${@}
+install_humhub_with_onlyoffice(){
+  source /app/common/install_dependencies.sh
+  install_dependencies
+  prepare_connector
+  prepare_files
+  docker-compose -f /app/humhub/docker-compose.yml up -d 
+  echo OK > /opt/run
+  echo -e "\e[0;32m Installation is complete \e[0m"
+}
+prepare_connector(){
+  source /app/common/get_connector.sh
+  get_connector
+  unzip /connectors/${CONNECTOR_NAME} -d /app/humhub/modules
+}
+prepare_files() {
+  sed -i -e  "s!mriedmann/humhub!mriedmann/humhub:${SERVICE_TAG}!g" /app/humhub/docker-compose.yml
+}
+check_ready() {
+
+for i in {1..30}; do
+  curl -f -s http://localhost
+  if [[ "$?" != "0" ]]; then
+	echo -e "\e[0;32m Waiting for the launch of humhub \e[0m"
+      sleep 10
+    else
+      echo -e "\e[0;32m humhub works \e[0m"
+      humhub_started="true"
+      break
+  fi
+done
+
+if [[ "$humhub_started" != 'true' ]]; then
+  echo -e "\e[0;31m I didn't wait for the launch of humhub. \e[0m"
+  exit 1
+fi
+
+for i in {1..30}; do
+  if [[ "$(curl -f -s http://localhost/ds-vpath/healthcheck)" != "true" ]]; then
+	echo -e "\e[0;32m Waiting for the launch of document-server \e[0m"
+      sleep 10
+    else
+      echo -e "\e[0;32m document-server is running \e[0m"
+      ds_started="true"
+      break
+  fi
+done
+
+if [[ "$ds_started" != 'true' ]]; then
+  echo -e "\e[0;31m Document server did not start. Check the container logs using the command: sudo docker logs -f onlyoffice-document-server. \e[0m"
+  exit 1
+fi
+}
+complete_installation(){
+  EXT_IP=`wget -q -O - ifconfig.me/ip`
+  echo -e "\e[0;32m Then you can go to the humhub web interface at: http://$EXT_IP and check the connector operation. \e[0m"
+  echo -e "\e[0;32m The script is finished \e[0m"
+}
+install_humhub_with_onlyoffice
+check_ready
+complete_installation
---
+++ humhub/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -x
+
+sed -i "$((`grep -n 'http {' /etc/nginx/nginx.conf | awk -F':' '{print $1}'`+1))i\ \ \ \ include       /tmp/nginx_conf/map.conf;" /etc/nginx/nginx.conf
+sed -i "$((`grep -n 'server {' /etc/nginx/nginx.conf | awk -F':' '{print $1}'`+1))i\ \ \ \ \ \ \ \ include       /tmp/nginx_conf/local.conf;" /etc/nginx/nginx.conf
+
+supervisord -n -c /etc/supervisord.conf &> /tmp/server.log &
+sleep 10
+cd /var/www/localhost/htdocs/protected
+./yii module/enable onlyoffice
+./yii settings/set onlyoffice serverUrl /ds-vpath/
+./yii settings/set onlyoffice internalServerUrl http://ds
+./yii settings/set onlyoffice storageUrl http://humhub
+tail -f /tmp/server.log
