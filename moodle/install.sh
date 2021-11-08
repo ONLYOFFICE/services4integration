bde41a24ab55e89d3458704b4f043d8e7a0dc75f
AlexandrKharitonov <75480173+AleksandrKharitonov@users.noreply.github.com>
2021-11-08 16:56:11 +0300

    prepair install.sh

--- moodle/install.sh
+++ moodle/install.sh
@@ -1,50 +1,53 @@
 #!/bin/bash
-# Prepares a HumHub stand with an installed connector. 
+# Prepares a Moodle stand with an installed connector. 
 
-SERVICE_TAG="stable"
-CONNECTOR_URL="https://github.com/ONLYOFFICE/onlyoffice-humhub/releases/\
-download/v2.2.2/onlyoffice.zip"
+SERVICE_TAG="latest"
+CONNECTOR_URL="https://github.com/ONLYOFFICE/onlyoffice-moodle/archive/\
+refs/tags/v1.0.0.tar.gz"
 CONNECTOR_NAME="${CONNECTOR_URL##*/}"
 source /app/common/error.sh
 source /app/common/check_parameters.sh $@
-install_humhub_with_onlyoffice(){
+install_moodle_with_onlyoffice(){
   source /app/common/install_dependencies.sh
   install_dependencies
   prepare_connector
   prepare_files
-  docker-compose -f /app/humhub/docker-compose.yml up -d 
+  docker-compose -f /app/moodle/docker-compose.yml up -d 
   echo OK > /opt/run
   echo -e "\e[0;32m Installation is complete \e[0m"
 }
 prepare_connector(){
   source /app/common/get_connector.sh
   get_connector
-  apt install unzip -y
-  unzip /connectors/${CONNECTOR_NAME} -d /app/humhub/modules
+  tar -C /app/moodle -xvf /connectors/${CONNECTOR_NAME}
+  mv /app/moodle/onlyoffice* /app/moodle/onlyoffice
 }
 prepare_files() {
-  sed -i -e  "s!mriedmann/humhub!mriedmann/humhub:${SERVICE_TAG}!g" \
-    /app/humhub/docker-compose.yml
+  EXT_IP=$(wget -q -O - ifconfig.me/ip)
+  sed -i -e "s!bitnami/moodle!bitnami/moodle:${SERVICE_TAG}!g" \
+    /app/moodle/docker-compose.yml
+  sed -i -e "s!https://documentserver.url!http://${EXT_IP}/ds-vpath/!g" \
+    /app/moodle/onlyoffice/settings.php
 }
 check_ready() {
-  local humhub_started
+  local moodle_started
   local healthcheck_ds
   local ds_started
 
   for i in {1..30}; do
-    curl -f -s http://localhost
+    curl -f -s http://localhost > /dev/null
     if [[ "$?" -ne 0 ]]; then
-      echo -e "\e[0;32m Waiting for the launch of humhub \e[0m"
+      echo -e "\e[0;32m Waiting for the launch of moodle \e[0m"
         sleep 10
       else
-        echo -e "\e[0;32m humhub works \e[0m"
-        humhub_started='true'
+        echo -e "\e[0;32m moodle works \e[0m"
+        moodle_started='true'
         break
     fi
   done
 
-  if [[ "$humhub_started" != 'true' ]]; then
-    err "\e[0;31m I didn't wait for the launch of humhub. \e[0m"
+  if [[ "$moodle_started" != 'true' ]]; then
+    err "\e[0;31m I didn't wait for the launch of moodle. \e[0m"
     exit 1
   fi
 
@@ -68,13 +71,12 @@ sudo docker logs -f onlyoffice-document-server. \e[0m"
   fi
 }
 complete_installation(){
-  EXT_IP=$(wget -q -O - ifconfig.me/ip)
-  echo -e "\e[0;32m Then you can go to the humhub web interface at: \
+  echo -e "\e[0;32m Then you can go to the moodle web interface at: \
 http://$EXT_IP and check the connector operation. \e[0m"
   echo -e "\e[0;32m The script is finished \e[0m"
 }
 main() {
-  install_humhub_with_onlyoffice
+  install_moodle_with_onlyoffice
   check_ready
   complete_installation
 }
