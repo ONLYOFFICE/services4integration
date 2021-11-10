c52b6e35f1b1b2d86f9cd096f5c5e54f5930f3d1
AlexandrKharitonov <75480173+AleksandrKharitonov@users.noreply.github.com>
2021-11-10 13:07:13 +0300

    format file

--- moodle/install.sh
+++ moodle/install.sh
@@ -7,7 +7,7 @@ refs/tags/v1.0.0.tar.gz"
 CONNECTOR_NAME="${CONNECTOR_URL##*/}"
 source /app/common/error.sh
 source /app/common/check_parameters.sh $@
-install_moodle_with_onlyoffice(){
+install_moodle_with_onlyoffice() {
   source /app/common/install_dependencies.sh
   install_dependencies
   prepare_connector
@@ -16,14 +16,14 @@ install_moodle_with_onlyoffice(){
   echo OK > /opt/run
   echo -e "\e[0;32m Installation is complete \e[0m"
 }
-prepare_connector(){
+prepare_connector() {
   source /app/common/get_connector.sh
   get_connector
   tar -C /app/moodle -xvf /connectors/${CONNECTOR_NAME}
   mv /app/moodle/onlyoffice* /app/moodle/onlyoffice
 }
 prepare_files() {
-  EXT_IP=$(wget -q -O - ifconfig.me/ip)
+  readonly EXT_IP=$(wget -q -O - ifconfig.me/ip)
   sed -i -e "s!bitnami/moodle!bitnami/moodle:${SERVICE_TAG}!g" \
     /app/moodle/docker-compose.yml
   sed -i -e "s!https://documentserver.url!http://${EXT_IP}/ds-vpath/!g" \
@@ -39,40 +39,40 @@ check_ready() {
     if [[ "$?" -ne 0 ]]; then
       echo -e "\e[0;32m Waiting for the launch of moodle \e[0m"
         sleep 10
-      else
-        echo -e "\e[0;32m moodle works \e[0m"
-        moodle_started='true'
-        break
+    else
+      echo -e "\e[0;32m moodle works \e[0m"
+      moodle_started='true'
+      break
     fi
   done
 
-  if [[ "$moodle_started" != 'true' ]]; then
+  if [[ "${moodle_started}" != 'true' ]]; then
     err "\e[0;31m I didn't wait for the launch of moodle. \e[0m"
     exit 1
   fi
 
   for i in {1..30}; do
     healthcheck_ds="$(curl -f -s http://localhost/ds-vpath/healthcheck)"
-    if [[ "$healthcheck_ds" != 'true' ]]; then
+    if [[ "${healthcheck_ds}" != 'true' ]]; then
       echo -e "\e[0;32m Waiting for the launch of document-server \e[0m"
         sleep 10
-      else
-        echo -e "\e[0;32m document-server is running \e[0m"
-        ds_started='true'
-        break
+    else
+      echo -e "\e[0;32m document-server is running \e[0m"
+      ds_started='true'
+      break
     fi
   done
 
-  if [[ "$ds_started" != 'true' ]]; then
+  if [[ "${ds_started}" != 'true' ]]; then
     err "\e[0;31m Document server did not start. \
 Check the container logs using the command: \
 sudo docker logs -f onlyoffice-document-server. \e[0m"
     exit 1
   fi
 }
-complete_installation(){
+complete_installation() {
   echo -e "\e[0;32m Then you can go to the moodle web interface at: \
-http://$EXT_IP and check the connector operation. \e[0m"
+http://${EXT_IP} and check the connector operation. \e[0m"
   echo -e "\e[0;32m The script is finished \e[0m"
 }
 main() {
