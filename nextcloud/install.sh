92b5016f86a52bc42a121e48e1f6d25482f3c429
Anton Rychenkov <anton.rychenkov@onlyoffice.com>
2021-11-10 11:18:24 +0400

    Refactoring to Google Shell Style

--- nextcloud/install.sh
+++ nextcloud/install.sh
@@ -6,8 +6,8 @@ source /app/common/check_parameters.sh ${@}
 source /app/common/get_connector.sh
 
 install_nextcloud() {
-  export SERVICE_TAG=`echo $SERVICE_TAG`
-  export EXT_IP=`wget -q -O - ifconfig.me/ip`  
+  export SERVICE_TAG="$(echo "$SERVICE_TAG")"
+  export EXT_IP="$(wget -q -O - ifconfig.me/ip)"
   source /app/common/install_dependencies.sh
   install_dependencies
   cd /app/nextcloud/
@@ -17,26 +17,24 @@ install_nextcloud() {
 install_connector() {
   get_connector
   cd /connectors
-  tar -xzf $CONNECTOR_NAME && rm -f $CONNECTOR_NAME
+  tar -xzf "$CONNECTOR_NAME" && rm -f "$CONNECTOR_NAME"
   docker cp /connectors/onlyoffice nextcloud_app:/var/www/html/apps
   docker exec -d nextcloud_app sh -c "chown -R www-data:www-data apps/onlyoffice"
 }
 
 check_ready() {
-for ((i=30; i>0 ; i--))
-  do
-    if [[ "$(curl --connect-timeout 2 -L -s -o /dev/null -w ''%{http_code}'' http://localhost:8080)" != "200" ]]
-      then 
-        echo Waiting to ready
-        sleep 10
-      else
-        echo Nextcloud is up on 
-        echo http://$EXT_IP:8080
-        return 1
-    fi
-  done
-    echo Nextcloud is unavailable
-    exit 1
+for ((i=30; i>0 ; i--)); do
+  if [[ "$(curl --connect-timeout 2 -L -s -o /dev/null -w ''%{http_code}'' http://localhost:8080)" != "200" ]]; then 
+    echo "Waiting to ready"
+    sleep 10
+  else
+    echo "Nextcloud is up on" 
+    echo "http://$EXT_IP:8080"
+    return 1
+  fi
+done
+  echo "Nextcloud is unavailable"
+  exit 1
 }
 
 #main
