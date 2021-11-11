f822c5b23b584470032d77ca1a3504a87fc58e74
VyacheslavSemin <sla1733@yandex.ru>
2021-11-11 15:04:33 +0000

    Use a haproxy image with automatic node detection

--- jira/cluster/add_node.sh
+++ jira/cluster/add_node.sh
@@ -4,41 +4,43 @@ SERVICE_TAG=$(docker ps | grep -i ${JIRA_NODES[1]} | awk '{print$2}' | sed 's/[^
 ALLIPADDR="$(hostname -I)"
 declare -a IPADDR=($ALLIPADDR)
 IP_PROXY=${IPADDR[0]}
-add_second_node(){
+
+add_second_node() {
   export SERVICE_TAG="${SERVICE_TAG}"
   export IP_PROXY="${IP_PROXY}"
   export JIRA_NODE_ID=jira_node_2
   cd /app/jira/cluster/
   envsubst < docker-compose.yml | docker-compose -f - up -d --scale jira-node=2 --no-recreate
 }
-check_cluster_readiness(){
+
+check_cluster_readiness() {
   echo -e "\e[0;32m Wait until the Jira cluster nodes are ready \e[0m"
   for name in "${JIRA_NODES[@]}"; do
-    for i in {1..100}; do
+    for ((i=1 ; i <= 100 ; i++)); do
       echo "Waiting for $name to be ready: $i"
       docker logs ${name} | grep -w "Warmed cache(s)"
       if [ $? -ne 0 ]; then
-        if [[ "$i" == '99' ]]; then
-          echo -e "\e[0;31m I didn't wait for the launch of $name. Check the container logs using the command: sudo docker logs -f $name \e[0m"
-          exit 1
-        else
-          sleep 5
-        fi
+        sleep 5
       else
         echo -e "\e[0;32m Node $name is ready \e[0m"
         if [[ "$name" == "${JIRA_NODES[0]}" ]]; then
           docker restart ${JIRA_NODES[1]}
         fi
+        NODE_READY="yes"
         break
       fi
     done
+    if [[ "$NODE_READY" != "yes" ]]; then
+     echo -e "\e[0;31m I didn't wait for the launch of $name. Check the container logs using the command: sudo docker logs -f $name \e[0m"
+     exit 1
+    fi
   done
 }
-complete_cluster_construction(){
-  sed -i 's/[^.]*server s2/   server s2/g' /jira/haproxy/haproxy.cfg
-  docker restart haproxy
+
+complete_cluster_construction() {
   echo -e "\e[0;32m Cluster nodes have been added and are ready to work. In the System - Clustering menu, you can check their status \e[0m"
 }
+
 add_second_node
 check_cluster_readiness
 complete_cluster_construction
--- jira/cluster/install.sh
+++ jira/cluster/install.sh
@@ -7,84 +7,85 @@ ALLIPADDR="$(hostname -I)"
 declare -a IPADDR=($ALLIPADDR)
 IP_PROXY=${IPADDR[0]}
 source /app/common/check_parameters.sh ${@}
-install_jira(){
+
+install_jira() {
   source /app/common/install_dependencies.sh
   install_dependencies
   export SERVICE_TAG="${SERVICE_TAG}"
   export IP_PROXY="${IP_PROXY}"
   export JIRA_NODE_ID=jira_node_1
-  mkdir -p /jira/haproxy
   mkdir -p /jira/share/plugins/installed-plugins
   chown -R 2001:2001 /jira/share/
-  cp /app/jira/cluster/haproxy.cfg /jira/haproxy/
   docker network create --driver bridge onlyoffice
   cd /app/jira/cluster/
   envsubst < docker-compose.yml | docker-compose -f - up -d
   echo OK > /opt/run
   echo -e "\e[0;32m Installation is complete \e[0m"
 }
-check_launch_jira(){
+
+check_launch_jira() {
   echo -e "\e[0;32m Waiting for the launch of Jira \e[0m"
-  for name in "${JIRA_NODES[@]}"; do
-    for i in {1..50}; do
-      echo "Getting the $name status: $i"
-      docker logs ${name} | grep -w "Jira is ready to serve"
-      if [ $? -ne 0 ]; then
-        if [[ "$i" == '49' ]]; then
-          echo -e "\e[0;31m I didn't wait for the launch of $name. Check the container logs using the command: sudo docker logs -f $name \e[0m"
-          exit 1
-        else
-          sleep 5
-        fi
-      else
-        echo -e "\e[0;32m Jira Node $name works \e[0m"
-        break
-      fi
-    done
+  for ((i=1 ; i <= 100 ; i++)); do
+    echo "Getting the ${JIRA_NODES} status: $i"
+    docker logs ${JIRA_NODES} | grep -w "Jira is ready to serve"
+    if [ $? -ne 0 ]; then
+      sleep 5
+    else
+      echo -e "\e[0;32m Jira Node ${JIRA_NODES} works \e[0m"
+      NODE_READY="yes"
+      break
+    fi
   done
+  if [[ "$NODE_READY" != "yes" ]]; then
+    echo -e "\e[0;31m I didn't wait for the launch of ${JIRA_NODES}. Check the container logs using the command: sudo docker logs -f ${JIRA_NODES} \e[0m"
+    exit 1
+  fi
 }
-prepare_connector(){
+
+prepare_connector() {
   source /app/common/get_connector.sh
   get_connector
   cp /connectors/${CONNECTOR_NAME} /jira/share/plugins/installed-plugins/
   chown -R 2001:2001 /jira/share/
 }
-check_connector_in_container(){
-  for name in "${JIRA_NODES[@]}"; do
-    docker exec ${name} bash -c 'test -e /data/jira/sharedhome/plugins/installed-plugins/${CONNECTOR_NAME}'
-    if [ $? -ne 0 ]; then
-      echo -e "\e[0;31m The connector under test was not added to the container in the /data/jira/sharedhome/plugins/installed-plugins directory \e[0m"
-      exit 1
-    else
-      echo -e "\e[0;32m The connector was successfully added to ${name} \e[0m"
-    fi
-  done
+
+check_connector_in_container() {
+  docker exec ${JIRA_NODES} bash -c 'test -e /data/jira/sharedhome/plugins/installed-plugins/${CONNECTOR_NAME}'
+  if [ $? -ne 0 ]; then
+    echo -e "\e[0;31m The connector under test was not added to the container in the /data/jira/sharedhome/plugins/installed-plugins directory \e[0m"
+    exit 1
+  else
+    echo -e "\e[0;32m The connector was successfully added \e[0m"
+  fi
 }
-database_initialization(){
+
+database_initialization() {
   echo "Database initialization begins. Please wait..."
   sleep 25
   curl -L http://${IPADDR[0]}
-  for i in {1..50}; do
+  for ((i=1 ; i <= 100 ; i++)); do
     echo "Readiness check: $i"
     docker logs ${JIRA_NODES} | grep -w "Upgrade Succeeded! JIRA has been upgraded"
     if [ $? -ne 0 ]; then
-      if [[ "$i" == '49' ]]; then
-        echo -e "\e[0;31m Readiness check failed. Check the container logs using the command: sudo docker logs -f ${JIRA_NODES} \e[0m"
-        exit 1
-      else
-        sleep 5
-      fi
+      sleep 5
     else
       echo -e "\e[0;32m Initialization completed successfully \e[0m"
+      DATABASE_READY="yes"
       break
     fi
   done
+  if [[ "$DATABASE_READY" != "yes" ]]; then
+    echo -e "\e[0;31m Readiness check failed. Check the container logs using the command: sudo docker logs -f ${JIRA_NODES} \e[0m"
+    exit 1
+  fi
 }
-complete_installation(){
+
+complete_installation() {
   echo -e "\e[0;32m The script is finished \e[0m"
   echo -e "\e[0;32m Jira is ready for further configuration \e[0m"
   echo -e "\e[0;32m Now you can go to the Jira web interface at http://${IPADDR[0]}/ and follow a few configuration steps \e[0m"
 }
+
 install_jira
 check_launch_jira
 prepare_connector
