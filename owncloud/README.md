18d6ec9616162ace3ea127ba68db5404c4746725
AleksandrKharitonov <75480173+AleksandrKharitonov@users.noreply.github.com>
2021-11-16 11:28:02 +0300

    Add installation of a owncloud stand (#10)
    * add owncloud script
    
    * fix install jq
    
    * fix final message
    
    * fix check
    
    * format file
    
    * add err.sh

---
+++ owncloud/README.md
@@ -0,0 +1,30 @@
+# Stand for testing the connector to owncloud
+
+Pass the following `cloud-init` directives to the instance with `user data`:
+```
+#cloud-config
+
+runcmd:
+ - git clone --depth=1 https://username:password@github.com/ONLYOFFICE/services4integration.git /app
+ - bash /app/owncloud/install.sh -st owncloud_tag -cu connector_url
+```
+
+Where:
+ - `owncloud_tag` - owncloud version. The available versions of owncloud can be viewed [here](https://hub.docker.com/r/owncloud/server/tags)
+ - `connector_url` - The address at which the connector under test is available. The available versions of the connector can be viewed [here](https://github.com/ONLYOFFICE/onlyoffice-owncloud/releases)
+
+For example:
+```
+/app/owncloud/install.sh -st latest -cu https://github.com/ONLYOFFICE/onlyoffice-owncloud/releases/download/v7.1.1/onlyoffice.tar.gz
+```
+
+After that, you can connect via SSH to the VM and check the progress of the script using the following command:
+```
+sudo tail -f /var/log/cloud-init-output.log
+```
+
+If successful, the following line will appear:
+``` 
+The script is finished
+```
+Then you can go to the owncloud web interface at: `http://IP-SERVER/` and check the connector operation.
---
+++ owncloud/install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Prepares a owncloud stand with an installed connector. 
+
+SERVICE_TAG="latest"
+CONNECTOR_URL="https://github.com/ONLYOFFICE/onlyoffice-owncloud/releases/\
+download/v7.1.1/onlyoffice.tar.gz"
+CONNECTOR_NAME="${CONNECTOR_URL##*/}"
+source /app/common/error.sh
+source /app/common/check_parameters.sh $@
+install_owncloud_with_onlyoffice() {
+  source /app/common/install_dependencies.sh
+  install_dependencies
+  git clone https://github.com/ONLYOFFICE/docker-onlyoffice-owncloud /apps
+  prepare_connector
+  prepare_files
+  docker-compose -f /apps/docker-compose.yml up -d 
+  echo OK > /opt/run
+  echo -e "\e[0;32m Installation is complete \e[0m"
+}
+prepare_connector() {
+  source /app/common/get_connector.sh
+  get_connector
+  tar -C /apps -xvf /connectors/${CONNECTOR_NAME}
+}
+prepare_files() {
+  sed -i -e  "s!owncloud/server!owncloud/server:${SERVICE_TAG}!g" \
+    /apps/docker-compose.yml
+}
+check_ready() {
+  local owncloud_started
+  local healthcheck_ds
+  local ds_started  
+
+  for i in {1..30}; do
+    curl -f -s http://localhost > /dev/null
+    if [[ "$?" -ne 0 ]]; then
+      echo -e "\e[0;32m Waiting for the launch of owncloud \e[0m"
+        sleep 10
+    else
+      echo -e "\e[0;32m owncloud works \e[0m"
+      owncloud_started='true'
+      break
+    fi
+  done
+
+  if [[ "${owncloud_started}" != 'true' ]]; then
+    err "\e[0;31m I didn't wait for the launch of owncloud. \e[0m"
+    exit 1
+  fi
+
+  for i in {1..30}; do
+    healthcheck_ds="$(curl -f -s http://localhost/ds-vpath/healthcheck)"
+    if [[ "${healthcheck_ds}" != 'true' ]]; then
+      echo -e "\e[0;32m Waiting for the launch of document-server \e[0m"
+        sleep 10
+    else
+      echo -e "\e[0;32m document-server is running \e[0m"
+      ds_started='true'
+      break
+    fi
+  done
+
+  if [[ "${ds_started}" != 'true' ]]; then
+    err "\e[0;31m Document server did not start. \
+Check the container logs using the command: \
+sudo docker logs -f onlyoffice-document-server. \e[0m"
+    exit 1
+  fi
+}
+complete_installation() {
+  readonly EXT_IP=$(wget -q -O - ifconfig.me/ip)
+  
+  echo -e "\e[0;32m Then you can go to the owncloud web interface at: \
+http://${EXT_IP} and check the connector operation. \e[0m"
+  echo -e "\e[0;32m The script is finished \e[0m"
+}
+main() {
+  install_owncloud_with_onlyoffice
+  check_ready
+  complete_installation
+}
+main
