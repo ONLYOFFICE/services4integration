52b5571ba63a8134716d774826ef0105de00a658
AleksandrKharitonov <75480173+AleksandrKharitonov@users.noreply.github.com>
2021-11-16 11:28:45 +0300

    Add installation of a humhub stand (#11)
    * Add installation of a humhub stand
    
    * formatted scripts
    
    * fix script
    
    * add install unzip
    
    * admin password changed
    
    * nginx_conf move to common
    
    * fix nginx_conf
    
    * fix connector conf
    
    * format file

---
+++ humhub/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# There is no bash in the container.
+# In a HumHub container:
+#   - a nginx.conf is being prepared for working with a document server;
+#   - the HumHub is launched;
+#   - the connector is activated and adjusted.
+
+set -x
+
+sed -i "$(($(grep -n 'http {' /etc/nginx/nginx.conf | \
+  awk -F':' '{print $1}')+1))i\    include       /tmp/nginx_conf/map.conf;" \
+  /etc/nginx/nginx.conf
+
+sed -i "$(($(grep -n 'server {' /etc/nginx/nginx.conf | \
+  awk -F':' '{print $1}')+1))i\        include       /tmp/nginx_conf/local.conf;" \
+  /etc/nginx/nginx.conf
+
+supervisord -n -c /etc/supervisord.conf &> /tmp/server.log &
+sleep 10
+cd /var/www/localhost/htdocs/protected
+./yii module/enable onlyoffice
+./yii settings/set onlyoffice serverUrl /ds-vpath/
+./yii settings/set onlyoffice internalServerUrl http://onlyoffice-document-server
+./yii settings/set onlyoffice storageUrl http://app-server/
+tail -f /tmp/server.log
