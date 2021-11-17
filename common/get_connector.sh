047764823d16059361c5198070777b7e85ba9cb7
VyacheslavSemin <sla1733@yandex.ru>
2021-11-17 19:07:55 +0000

    Remake external modules by style guide

--- common/get_connector.sh
+++ common/get_connector.sh
@@ -1,5 +1,17 @@
-get_connector(){
-  echo -e "\e[0;32m The connector will now be added to the container \e[0m"
+##############################################################
+# Get a connector
+# Globals:
+#   CONNECTOR_NAME
+#   CONNECTOR_URL
+# Arguments:
+#   None
+# Outputs:
+#   Write the status of receiving the connector to stdout
+# Returns:
+#   0 if thing was received, non-zero on error
+##############################################################
+get_connector() {
+  echo -e "\e[0;32m The connector will now be downloaded to the host \e[0m"
   mkdir /connectors
   wget -O /connectors/${CONNECTOR_NAME} ${CONNECTOR_URL}
   if [ ! -f "/connectors/${CONNECTOR_NAME}" ]; then
@@ -7,8 +19,8 @@ get_connector(){
     exit 1
   fi
   CONNECTOR_SIZE="$(du /connectors/${CONNECTOR_NAME} | awk '{print $1}')"
-  echo "$CONNECTOR_SIZE"
-  if [[ "$CONNECTOR_SIZE" == '0' ]]; then
+  echo "${CONNECTOR_SIZE}"
+  if [[ "${CONNECTOR_SIZE}" == '0' ]]; then
     echo -e "\e[0;31m The size of the connector is 0, check that the connector is loaded correctly \e[0m"
     exit 1
   fi
