f5c9cd5c0121865f3de42b017b7998fa3217aa80
Vyacheslav Semin <sla1733@yandex.ru>
2021-11-18 11:36:02 +0300

    Remake external modules by style guide (#15)

--- common/check_parameters.sh
+++ common/check_parameters.sh
@@ -1,20 +1,27 @@
+###############################################################################################
+# Check the passed parameters and their values and assign the received values to variables
+# Arguments:
+#   Application version, path to the connector under test
+# Outputs:
+#   If there are no parameters, writes a message to stdout
+###############################################################################################
 if [ "$1" == "" ]; then
   echo -e "\e[0;33m Warning: Basic parameters are missing. The default values will be used \e[0m"
 fi
 while [ "$1" != "" ]; do
   case $1 in
     -st | --service_tag )
-       if [ "$2" != "" ]; then
-         SERVICE_TAG=$2
-         shift
-       fi
-    ;;
+      if [ "$2" != "" ]; then
+        SERVICE_TAG=$2
+        shift
+      fi
+      ;;
     -cu | --connector_url )
-       if [ "$2" != "" ]; then
-         CONNECTOR_URL=$2
-         shift
-       fi
-    ;;
+      if [ "$2" != "" ]; then
+        CONNECTOR_URL=$2
+        shift
+      fi
+      ;;
   esac
   shift
 done
--- common/install_dependencies.sh
+++ common/install_dependencies.sh
@@ -1,4 +1,9 @@
-install_dependencies(){
+#####################################################
+# Install the necessary dependencies on the host
+# Arguments:
+#   None
+#####################################################
+install_dependencies() {
   apt-get update && apt-get install -y apt-transport-https curl wget ca-certificates software-properties-common gnupg2 ntp net-tools
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
