b621c46b392bc1010456907f3ec2b8aa7e279bfd
AleksandrKharitonov <75480173+AleksandrKharitonov@users.noreply.github.com>
2021-11-22 11:21:13 +0300

    Add installation of a Plone stand (#16)
    * Add install file
    
    * Add install_dependencies funcion to install script
    
    * add docker-compose
    
    * update task
    
    * fix replace
    
    * fix healthcheck
    
    * delete conflicting files
    
    Co-authored-by: Sergey Malkin <sergey.malkin@onlyoffice.com>

---
+++ plone/README.md
@@ -0,0 +1,30 @@
+# Stand for testing the connector to Plone
+
+Pass the following `cloud-init` directives to the instance with `user data`:
+```
+#cloud-config
+
+runcmd:
+ - git clone --depth=1 https://username:password@github.com/ONLYOFFICE/services4integration.git /app
+ - bash /app/plone/install.sh -st plone_tag -cu connector_url
+```
+
+Where:
+ - `plone_tag` - plone version. The available versions of plone can be viewed [here](https://hub.docker.com/_/plone?tab=tags)
+ - `connector_url` - The address at which the connector under test is available. The available versions of the connector can be viewed [here](https://github.com/ONLYOFFICE/onlyoffice-plone/releases)
+
+For example:
+```
+/app/plone/install.sh -st latest -cu https://github.com/ONLYOFFICE/onlyoffice-plone/archive/refs/tags/v1.0.0.tar.gz
+```
+
+After that, you can connect via SSH to the VM and check the progress of the script using the following command:
+```
+sudo tail -f /var/log/cloud-init-output.log
+```
+
+If successful, the following line will appear:
+``` 
+The script is finished
+```
+Then you can go to the plone web interface at: `http://IP-SERVER/` and check the connector operation.
---
+++ plone/install.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Prepares a Plone stand with an installed connector. 
+
+SERVICE_TAG="latest"
+CONNECTOR_URL="https://github.com/ONLYOFFICE/onlyoffice-plone/releases/\
+download/v2.0.0/onlyoffice.connector-2.0.0.tar.gz"
+CONNECTOR_NAME="${CONNECTOR_URL##*/}"
+source /app/common/error.sh
+source /app/common/check_parameters.sh $@
+install_plone_with_onlyoffice() {
+  source /app/common/install_dependencies.sh
+  install_dependencies
+  prepare_connector
+  prepare_files
+  docker-compose -f /app/plone/docker-compose.yml up -d 
+  echo OK > /opt/run
+  echo -e "\e[0;32m Installation is complete \e[0m"
+}
+prepare_connector() {
+  source /app/common/get_connector.sh
+  get_connector
+  tar -C /app/plone -xvf /connectors/${CONNECTOR_NAME}
+  mv /app/plone/onlyoffice* /app/plone/onlyoffice
+}
+prepare_files() {
+  readonly EXT_IP=$(wget -q -O - ifconfig.me/ip)
+  sed -i -e "s!plone:latest!plone:${SERVICE_TAG}!g" \
+    /app/plone/docker-compose.yml
+  sed -i -e "s!https://documentserver/!http://${EXT_IP}/ds-vpath/!g" \
+    /app/plone/onlyoffice/src/onlyoffice/connector/browser/controlpanel.py
+}
+check_ready() {
+  local plone_started
+  local healthcheck_ds
+  local ds_started
+
+  for i in {1..30}; do
+    curl -f -s http://localhost > /dev/null
+    if [[ "$?" -ne 0 ]]; then
+      echo -e "\e[0;32m Waiting for the launch of plone \e[0m"
+        sleep 10
+    else
+      echo -e "\e[0;32m plone works \e[0m"
+      plone_started='true'
+      break
+    fi
+  done
+
+  if [[ "${plone_started}" != 'true' ]]; then
+    err "\e[0;31m I didn't wait for the launch of plone. \e[0m"
+    exit 1
+  fi
+
+  for i in {1..30}; do
+    healthcheck_ds="$(curl -f -s http://localhost/ds-vpath/healthcheck)"
+    if [[ "${healthcheck_ds}" != 'true' ]]; then
+      echo -e "\e[0;32m Waiting for the launch of document-server \e[0m"
+        sleep 10
+    else
+      echo -e "\e[0;32m document-server is running \e[0m"
+      ds_started='true'
+      break
+    fi
+  done
+
+  if [[ "${ds_started}" != 'true' ]]; then
+    err "\e[0;31m Document server did not start. \
+Check the container logs using the command: \
+sudo docker logs -f onlyoffice-document-server. \e[0m"
+    exit 1
+  fi
+}
+complete_installation() {
+  echo -e "\e[0;32m Then you can go to the plone web interface at: \
+http://${EXT_IP} and check the connector operation. \e[0m"
+  echo -e "\e[0;32m The script is finished \e[0m"
+}
+main() {
+  install_plone_with_onlyoffice
+  check_ready
+  complete_installation
+}
+main
