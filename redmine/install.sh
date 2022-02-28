26483b509cd17e5066c4ca06a56924d0c1282ab5
kireevdmitry <octavarium78@gmail.com>
2022-02-28 06:44:22 -0500

    Fix enviroment to docker-compose

--- redmine/install.sh
+++ redmine/install.sh
@@ -2,20 +2,20 @@
 CONNECTOR_URL=https://github.com/ONLYOFFICE/onlyoffice-redmine/releases/download/v1.1.0/onlyoffice-redmine.zip
 CONNECTOR_NAME=onlyoffice-redmine.zip
 SERVICE_TAG='latest'
+source /app/common/check_parameters.sh "${@}"
 
 install_redmine(){
-  source /app/common/check_parameters.sh "${@}"
   source /app/common/install_dependencies.sh
   source /app/common/get_connector.sh
 
-  check_parameters
   install_dependencies
   get_connector
   apt-get install unzip -y
   unzip /connectors/$CONNECTOR_NAME -d /connectors
   mv /connectors/onlyoffice-redmine /connectors/onlyoffice_redmine
-  echo "TAG=$SERVICE_TAG">/app/redmine/.env
-  docker-compose -f /app/redmine/docker-compose.yml up -d
+  export TAG="${SERVICE_TAG}"
+  cd /app/redmine/
+  envsubst < docker-compose.yml | docker-compose -f - up -d
 }
   complete_installation(){
   echo -e "\e[0;32m The script is finished \e[0m"
