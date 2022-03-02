5245301637225435f3d023ffdf9ad6f394b12321
kireevdmitry <octavarium78@gmail.com>
2022-03-02 08:13:25 -0500

    Add comments

--- redmine/install.sh
+++ redmine/install.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
+#
+# Prepare a stand with Redmine with a dependent service Onlyoffice Document Server and add a connector
+
 CONNECTOR_URL=https://github.com/ONLYOFFICE/onlyoffice-redmine/releases/download/v1.1.0/onlyoffice-redmine.zip
 CONNECTOR_NAME=onlyoffice-redmine.zip
 SERVICE_TAG='latest'
 source /app/common/check_parameters.sh "${@}"
 
+#############################################################################################
+# Install the necessary dependencies on the host and install Redmine and dependent service
+# Globals:
+#   SERVICE_TAG
+# Arguments:
+#   None
+# Outputs:
+#   None
+#############################################################################################
 install_redmine(){
   source /app/common/install_dependencies.sh
   source /app/common/get_connector.sh
@@ -17,10 +29,13 @@ install_redmine(){
   cd /app/redmine/
   envsubst < docker-compose.yml | docker-compose -f - up -d
 }
-  complete_installation(){
+complete_installation(){
   echo -e "\e[0;32m The script is finished \e[0m"
 }
 
+main(){
 install_redmine
 complete_installation
+}
 
+main
