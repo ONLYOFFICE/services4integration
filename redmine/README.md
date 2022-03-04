3aef468de71f2b480e40ec07b52623380a1d5c37
Dmitry Kireev <octavarium78@gmail.com>
2022-03-04 13:15:52 +0300

    Add installation of a redmine stand (#22)
    * Add script to install redmine
    
    * Add readme
    
    * Fix enviroment to docker-compose
    
    * Add comments
    
    * Add readiness check
    
    * Move to readiness check function
    
    * Add message for check status
    
    * Fix output message

---
+++ redmine/README.md
@@ -0,0 +1,30 @@
+# Stand for testing the connector to Redmine
+
+Pass the following `cloud-init` directives to the instance with `user data`:
+```
+#cloud-config
+
+runcmd:
+ - git clone --depth=1 https://github.com/ONLYOFFICE/services4integration.git /app
+ - /app/redmine/install.sh -st redmine_tag -cu connector_url
+```
+
+Where:
+ - `redmine_tag` - redmine version. The available versions of Redmine can be viewed [here](https://hub.docker.com/_/redmine?tab=tags)
+ - `connector_url` - The address at which the connector under test is available. The available versions of the connector can be viewed [here](https://github.com/ONLYOFFICE/onlyoffice-redmine/releases)
+
+For example:
+```
+/app/redmine/install.sh -st 4.2 -cu https://github.com/ONLYOFFICE/onlyoffice-redmine/releases/download/v1.1.0/onlyoffice-redmine.zip
+```
+
+After that, you can connect via SSH to the VM and check the progress of the script using the following command:
+```
+sudo tail -f /var/log/cloud-init-output.log
+```
+
+If successful, the following line will appear:
+``` 
+The script is finished
+```
+Then you can go to the Redmine web interface at: `http://IP-SERVER:3000/` and check the connector operation. Default login: Admin password: admin
---
+++ redmine/install.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+#
+# Prepare a stand with Redmine with a dependent service Onlyoffice Document Server and add a connector
+
+CONNECTOR_URL=https://github.com/ONLYOFFICE/onlyoffice-redmine/releases/download/v1.1.0/onlyoffice-redmine.zip
+CONNECTOR_NAME=onlyoffice-redmine.zip
+SERVICE_TAG='latest'
+IP=$(hostname -I)
+IP_ARR=($IP)
+source /app/common/check_parameters.sh "${@}"
+source /app/common/error.sh
+
+#############################################################################################
+# Install the necessary dependencies on the host and install Redmine and dependent service
+# Globals:
+#   SERVICE_TAG
+# Arguments:
+#   None
+# Outputs:
+#   None
+#############################################################################################
+install_redmine() {
+  source /app/common/install_dependencies.sh
+  source /app/common/get_connector.sh
+
+  install_dependencies
+  get_connector
+  apt-get install unzip -y
+  unzip /connectors/$CONNECTOR_NAME -d /connectors
+  mv /connectors/onlyoffice-redmine /connectors/onlyoffice_redmine
+  export TAG="${SERVICE_TAG}"
+  cd /app/redmine/
+  envsubst < docker-compose.yml | docker-compose -f - up -d
+}
+
+#############################################################################################
+# Check redmine startup and status
+# Globals:
+#   SERVICE_TAG
+#   OUTPUT
+# Outputs:
+#   Writes a startup message to stdout
+# Returns
+#   0, if the start is successful, non-zero on error
+#############################################################################################
+check_redmine() {
+  echo -e "\e[0;32m Waiting for the launch of Redmine \e[0m"  
+  for i in {1..15}; do
+    echo "Getting the Redmine status: ${i}"
+    OUTPUT="$(curl -Is http://${IP_ARR[0]}:3000/ | head -1 | awk '{ print $2 }')"
+    if [ "${OUTPUT}" == "200" ]; then
+      echo -e "\e[0;32m redmine is ready to serve \e[0m"
+      local REDMINE_READY
+      REDMINE_READY='yes'
+      break
+    else  
+      sleep 10
+    fi
+  done
+  if [[ "${REDMINE_READY}" != 'yes' ]]; then
+    err "\e[0;31m I didn't wait for the launch of Redmine. Check the container logs using the command: sudo docker logs -f redmine \e[0m"
+    exit 1
+  fi
+}
+
+complete_installation() {
+  echo -e "\e[0;32m The script is finished \e[0m"
+  echo -e "\e[0;32m Now you can go to the Redmie web interface at http://${IP_ARR[0]}:3000/ and follow a few configuration steps \e[0m"
+}
+
+main() {
+install_redmine
+check_redmine
+complete_installation
+}
+
+main
