62a6cfeddd1b0594c4235d55090f2095202b0e66
kireevdmitry <octavarium78@gmail.com>
2022-04-07 03:32:51 -0400

    Add installation of a wordpress stand

---
+++ wordpress/README.md
@@ -0,0 +1,29 @@
+# Stand for testing the connector to Wordpress
+
+Pass the following `cloud-init` directives to the instance with `user data`:
+```
+#cloud-config
+
+runcmd:
+ - git clone --depth=1 https://github.com/ONLYOFFICE/services4integration.git /app
+ - /app/wordpress/install.sh -st wordpress_tag
+```
+
+Where:
+ - `ordpress_tag` - Wordpress version. The available versions of Wordpress can be viewed [here](https://hub.docker.com/_/wordpress?tab=tags)
+
+For example:
+```
+/app/wordpress/install.sh -st latest
+```
+
+After that, you can connect via SSH to the VM and check the progress of the script using the following command:
+```
+sudo tail -f /var/log/cloud-init-output.log
+```
+
+If successful, the following line will appear:
+``` 
+The script is finished
+```
+Then you can go to the Redmine web interface at: `http://IP-SERVER/` and check the connector operation.
---
+++ wordpress/install.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+#
+# Prepare a stand with Wordpress with a dependent service Onlyoffice Document Server and add a connector
+
+CONNECTOR_URL=''
+SERVICE_TAG='latest'
+IP=$(hostname -I)
+IP_ARR=($IP)
+source /app/common/check_parameters.sh "${@}"
+source /app/common/error.sh
+
+#############################################################################################
+# Install the necessary dependencies on the host and install Redmine and dependent service
+# Globals:
+#   SERVICE_TAG
+# Arguments:
+#   None
+# Outputs:
+#   None
+#############################################################################################
+install_wordpress() {
+  source /app/common/install_dependencies.sh
+  install_dependencies
+  mkdir -p /var/lib/wordpress/wp-content/plugins
+  git clone --branch=develop https://github.com/ONLYOFFICE/onlyoffice-wordpress.git /var/lib/wordpress/wp-content/plugins/onlyoffice-wordpress
+  export TAG="${SERVICE_TAG}"
+  cd /app/wordpress/
+  envsubst < docker-compose.yml | docker-compose -f - up -d
+}
+
+#############################################################################################
+# Check redmine startup and status
+# Globals:
+#   SERVICE_TAG
+# Outputs:
+#   Writes a startup message to stdout
+# Returns
+#   0, if the start is successful, non-zero on error
+#############################################################################################
+check_wordpress() {
+  echo -e "\e[0;32m Waiting for the launch of Wordpress \e[0m"  
+  for i in {1..15}; do
+    echo "Getting the Wordpress status: ${i}"
+    OUTPUT="$(curl -Is http://${IP_ARR[0]}/ | head -1 | awk '{ print $2 }')"
+    if [ "${OUTPUT}" == "200" ]; then
+      echo -e "\e[0;32m wordpress is ready to serve \e[0m"
+      local WORDPRESS_READY
+      WORDPRESS_READY='yes'
+      break
+    else  
+      sleep 10
+    fi
+  done
+  if [[ "${WORDPRESS_READY}" != 'yes' ]]; then
+    err "\e[0;31m I didn't wait for the launch of Wordpress. Check the container logs using the command: sudo docker logs -f wordpress \e[0m"
+    exit 1
+  fi
+}
+
+complete_installation() {
+  echo -e "\e[0;32m The script is finished \e[0m"
+  echo -e "\e[0;32m Now you can go to the Wordpress web interface at http://${IP_ARR[0]}/ and follow a few configuration steps \e[0m"
+}
+
+main() {
+  install_wordpress
+  check_wordpress
+  complete_installation
+}
