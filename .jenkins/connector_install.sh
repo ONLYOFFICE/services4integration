f42ff10cc4e748951ee52f0114d5cfc21a81cc94
Sergey Malkin <sergey.malkin@onlyoffice.com>
2022-07-04 14:17:22 +0300

    function create_vm was done

--- .jenkins/connector_install.sh
+++ .jenkins/connector_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SSH_KEY="/home/jenkins/connector_install_prv.ppk"
+SSH_KEY="/root/.ssh/ connector_install.pem"
 Connector=$1
 connector_url=$2
 
@@ -12,6 +12,10 @@ function create_vm() {
   -H "Authorization: Bearer ${DO_TOKEN}" \
   -d '{"name":"'${Connector}-${date}'","region":"fra1","size":"s-2vcpu-4gb","image":"ubuntu-20-04-x64","ssh_keys":[30223004,29633232,29102049,28963692,30876815,35168967],"backups":false,"ipv6":false,"user_data":null,"private_networking":null,"volumes": null,"tags":["connectors"]}' \
   "https://api.digitalocean.com/v2/droplets"
+}
+
+function check_vm_condition() {
+
 }
 
 function main() {
