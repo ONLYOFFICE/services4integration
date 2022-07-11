1f4b2a5b9b696906966a6bb3a7171dba886282fe
kireevdmitry <octavarium78@gmail.com>
2022-07-11 15:19:50 +0000

    Add strapi stand installation

---
+++ strapi/Readme.md
@@ -0,0 +1,30 @@
+# Stand for testing the connector to Strapi
+
+Pass the following `cloud-init` directives to the instance with `user data`:
+```
+#cloud-config
+
+runcmd:
+ - git clone --depth=1 https://github.com/ONLYOFFICE/services4integration.git /app
+ - /app/strapi/install.sh -st connector_version
+```
+
+Where:
+ - `connector_version` - The version at which the connector under test is available. The available versions of the connector can be viewed [here](https://github.com/ONLYOFFICE/onlyoffice-strapi/releases)
+
+For example:
+```
+/app/strapi/install.sh -st 1.0.6
+```
+
+After that, you can connect via SSH to the VM and check the progress of the script using the following command:
+```
+sudo tail -f /var/log/cloud-init-output.log
+```
+
+If successful, the following line will appear:
+``` 
+The script is finished
+```
+Then you can go to the Strapi web interface at: `http://IP-SERVER:1337/` and check the connector operation. 
+
---
+++ strapi/install.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+#
+# Prepare a stand with strapi with a dependent service Onlyoffice Document Server and add a connector
+
+SERVICE_TAG=""
+IP=$(hostname -I)
+IP_ARR=($IP)
+
+source /app/common/error.sh
+source /app/common/check_parameters.sh
+
+#############################################################################################
+# Install the necessary dependencies on the host and install strapi and dependent service
+# Globals:
+#   SERVICE_TAG
+# Arguments:
+#   None
+# Outputs:
+#   None
+#############################################################################################
+install_strapi() {
+source /app/common/install_dependencies.sh
+install_dependencies
+export DEBIAN_FRONTEND=noninteractive ; apt-get dist-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --force-yes
+apt install sqlite aptitude expect -y
+curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt install nodejs -y
+aptitude install npm -y
+npm install pm2 -g
+
+expect -c 'set timeout -1;
+spawn npx create-strapi-app@latest my-project --quickstart --no-run;
+expect "Ok to proceed? (y)";
+send -- "y\r";
+expect eof'
+
+sed -i 's!.*strapi::security.*!{\n  name: "strapi::security",\n  config: {\n    contentSecurityPolicy: {\n      useDefaults: true,\n      directives: {\n        "script-src": ["'self'", "https:", "http:"],\n        "frame-src": ["'self'", "https:", "http:"],\n      },\n    },\n  },\n},!' my-project/config/middlewares.js
+cd my-project
+if [ "$SERVICE_TAG" != "" ]; then
+  SERVICE_TAG=@$SERVICE_TAG
+fi
+npm install onlyoffice-strapi$SERVICE_TAG --save
+npm run build
+pm2 start "npm run develop"
+docker run -i -t -d -p 3000:80 --restart=always onlyoffice/documentserver
+}
+
+#############################################################################################
+# Check strapi startup and status
+# Globals:
+#   IP_ARR
+# Outputs:
+#   Writes a startup message to stdout
+# Returns
+#   0, if the start is successful, non-zero on error
+#############################################################################################
+check_strapi() {
+  echo -e "\e[0;32m Waiting for the launch of strapi \e[0m"  
+  for i in {1..30}; do
+    echo "Getting the strapi status: ${i}"
+    OUTPUT="$(curl -Is http://${IP_ARR[0]}:1337 | head -1 | awk '{ print $2 }')"
+    if [ "${OUTPUT}" == "200" ]; then
+      echo -e "\e[0;32m strapi is ready to serve \e[0m"
+      local STRAPI_READY
+      STRAPI_READY='yes'
+      break
+    else  
+      sleep 10
+    fi
+  done
+  if [[ "${STRAPI_READY}" != 'yes' ]]; then
+    err "\e[0;31m I didn't wait for the launch of strapi \e[0m"
+    exit 1
+  fi
+}
+
+complete_installation() {
+  echo -e "\e[0;32m The script is finished \e[0m"
+  echo -e "\e[0;32m Now you can go to the Strapi web interface at http://${IP_ARR[0]}:1337/ and follow a few configuration steps \e[0m"
+}
+
+main() {
+install_strapi
+check_strapi
+complete_installation
+}
+
+main
