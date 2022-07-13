c940b6382629d17f025c9f83154d0d778283a52d
Sergey Malkin <sergey.malkin@onlyoffice.com>
2022-07-13 10:16:02 +0300

    Feature/jenkinsfile (#34)
    * add jenkinsfile template
    
    * add creds
    
    * description
    
    * add hunhub
    
    * add sh file
    
    * ls for test
    
    * function create_vm
    
    * test1
    
    * function create_vm was done
    
    * humhub install with user_data.yml
    
    * next
    
    * change var
    
    * change user_data.yml
    
    * add bash to run install.sh
    
    * multi conditions
    
    * multi conditions test
    
    * connector install step to sh script
    
    * to sh
    
    * env to environment
    
    * move vars to scripts root
    
    * change user-data path
    
    * check droplet`s status
    
    * function check_web_status
    
    * upd
    
    * add sh to stage ('Delete old VM')
    
    * test 2
    
    * change CREATED_AT
    
    * check. if created_date is empty
    
    * increase check_web_status to 15 minuts
    
    * add all connectors
    
    * change jira tag
    
    * add other connectors to choice
    
    * null to connector_url
    
    * check public ip address
    
    * change jira tag
    
    * test 2
    
    * add port for web check
    
    * change jira port to 8080
    
    * add port to all connectors
    
    * change redmine latest tag
    
    * increase time to web check to 20 minuts
    
    * add alfresco
    
    * test 2
    
    * change alfresco vars
    
    * alfresco test 3
    
    * sed %space% to space
    
    * cu for chamilo
    
    * change cu
    
    * to st 2
    
    * add clean up stage
    
    * Update README.md
    
    * change plone`s cred
    
    * change droplet`s size to 4-8
    
    * add jira_cluster
    
    * change jiras name
    
    * add d_kireev ssh key-id
    
    * choice vm_size
    
    * delete debugging data

---
+++ .jenkins/connector_install.sh
@@ -0,0 +1,130 @@
+#!/usr/bin/env bash
+
+default_url=$1
+cu=$2
+connector_path=$3
+st=$4
+Connector=$5
+port=$6
+OUTPUT=".jenkins/output.json"
+ip_address=""
+droplet_name=""
+
+function droplet_status() {
+  for ((i=0; i<=15 ; i++))
+  do
+    status=$(curl -X GET -H "Content-Type: application/json" \
+      -H "Authorization: Bearer ${DO_TOKEN}" \
+      "https://api.digitalocean.com/v2/droplets/$1/actions" | jq .actions[].status | cut -f 2 -d'"') 
+
+    echo "$i attempt has status ${status}."
+    if [[ ${status} == "completed" ]]; then
+      return
+    else
+      sleep 20
+      continue
+    fi
+  done
+  
+  echo "Error! Droplet was not created during 5 minuts."
+  exit 1 
+}
+
+function create_vm() {
+  if [[ ${cu} == "from_pipeline" ]]; then
+    connector_url=${default_url}
+  else
+    connector_url=${cu}
+  fi
+
+  if [ -f "$OUTPUT" ]; then
+	  touch $OUTPUT;
+  fi
+
+  date=$(date '+%Y%m%d%H%M');
+
+  # prepare user_data
+  cp .jenkins/user-data.yml .jenkins/user-data.yml.tmp
+  tmp_path=".jenkins/user-data.yml.tmp"
+
+  sed -i "s,%connector_url%,${connector_url},g" ${tmp_path}
+  sed -i "s,%path%,${connector_path},g" ${tmp_path}
+  sed -i "s,%tag%,${st},g" ${tmp_path}
+  sed -i "s,%space%, ,g" ${tmp_path}
+
+  # create droplet
+  (curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer ${DO_TOKEN}" \
+  -d '{
+    "name":"'${Connector}-${date}'",
+    "region":"nyc3",
+    "size":"'${vm_size}'",
+    "image":"ubuntu-20-04-x64",
+    "ssh_keys":[30223004,29633232,29102049,28963692,30876815,35168967,30296916],
+    "backups":false,
+    "ipv6":false,
+    "user_data":"'" $(cat .jenkins/user-data.yml.tmp) "'",
+    "private_networking":null,
+    "volumes": null,
+    "tags":["connectors"]}' \
+  "https://api.digitalocean.com/v2/droplets" ) > ${OUTPUT}
+
+  # wait for os to start
+  droplet_name=$(jq ".droplet.name" $OUTPUT | cut -f 2 -d'"')
+  droplet_id=$(jq ".droplet.id" $OUTPUT | cut -f 2 -d'"')
+  droplet_status ${droplet_id}
+
+  echo "Droplet was created."
+}
+
+function check_vm_condition() {
+  # get droplets list with tag_name=connectors
+  (curl -X GET -H "Content-Type: application/json" \
+  -H "Authorization: Bearer ${DO_TOKEN}" \
+  "https://api.digitalocean.com/v2/droplets?tag_name=connectors") > $OUTPUT
+
+  # search droplet`s ip
+  for ((i=0; i<=30 ; i++))
+  do
+    NAME=$(jq ".droplets[$i].name" $OUTPUT | cut -f 2 -d'"')
+    IP=$(jq ".droplets[$i].networks.v4[0].ip_address" $OUTPUT | cut -f 2 -d'"')
+    TYPE=$(jq ".droplets[$i].networks.v4[0].type" $OUTPUT | cut -f 2 -d'"')
+
+    # get public ip address
+    if [[ ${TYPE} == "private" ]]; then
+      IP=$(jq ".droplets[$i].networks.v4[1].ip_address" $OUTPUT | cut -f 2 -d'"')
+    fi
+
+    if [[ ${NAME} == ${droplet_name} ]]; then
+      break
+    fi
+	done
+
+  ip_address=${IP}
+}
+
+function check_web_status() {
+  for ((i=0; i<=60 ; i++))
+  do
+    web_status=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${1})
+
+    if [[ ${web_status} = 200 ]]; then
+      return
+    else
+      echo "$i attempt. web site ${1} has status ${web_status}."
+      sleep 20
+      continue
+    fi
+  done 
+  
+  echo "Error! Droplet was not created during 20 minuts."
+  exit 1
+}
+
+function main() {
+  create_vm
+  check_vm_condition
+  check_web_status "http://${ip_address}:${port}"
+}
+
+main
