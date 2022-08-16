bd9ad8e96df7e1f37a6d89647d69fb29e6d99928
kireevdmitry <octavarium78@gmail.com>
2022-08-16 09:03:16 +0000

    Add seafile launch check

--- seafile/install.sh
+++ seafile/install.sh
@@ -54,6 +54,25 @@ check_app() {
     err "\e[0;31m I didn't wait for the launch of ${APP}. Check the container logs using the command: sudo docker logs -f seafile \e[0m"
     exit 1
   fi
+
+  for i in {1..30}; do
+    healthcheck_ds="$(curl -f -s http://${IP}:3000/healthcheck)"
+    if [[ "${healthcheck_ds}" != 'true' ]]; then
+      echo -e "\e[0;32m Waiting for the launch of document-server \e[0m"
+        sleep 10
+    else
+      echo -e "\e[0;32m document-server is running \e[0m"
+      ds_started='true'
+      break
+    fi
+  done
+
+  if [[ "${ds_started}" != 'true' ]]; then
+    err "\e[0;31m Document server did not start. \
+    Check the container logs using the command: \
+    sudo docker logs -f onlyoffice-document-server. \e[0m"
+    exit 1
+  fi
 }
 
 complete_installation() {
