e8658374db9ca7f2d455ee2001ea5d92e8b40076
danilapog <danil.titarenko@onlyoffice.com>
2022-09-13 13:48:37 +0300

    Add correct ready_check and comments

--- documentserver/install.sh
+++ documentserver/install.sh
@@ -6,12 +6,30 @@ SERVICE_TAG="latest"
 IP_ADDR_INT="$(hostname -I | awk '{print $1}')"
 IP_ADDR_EXT="$(wget -q -O - ifconfig.me/ip)"
 
+#############################################################################################
+# Install the necessary dependencies on the host and run DocumentServer 
+# Globals:
+#   SERVICE_TAG
+# Arguments:
+#   None
+# Outputs:
+#   None
+#############################################################################################
 install_onlyoffice_documentserver() {
   source /app/common/install_dependencies.sh
   install_dependencies
   docker run -i -t -d -p 80:80 --restart=always onlyoffice/documentserver:${SERVICE_TAG}
 }
 
+#############################################################################################
+# Check DocumentServer startup and status
+# Globals:
+#   None
+# Outputs:
+#   Writes a startup message to stdout
+# Returns
+#   0, if the start is successful, non-zero on error
+#############################################################################################
 ready_check() {
   echo -e "\e[0;32m Waiting for the launch of DocumentServer... \e[0m"  
   for i in {1..30}; do
@@ -19,11 +37,17 @@ ready_check() {
     OUTPUT="$(curl -Is http://localhost/healthcheck/ | head -1 | awk '{ print $2 }')"
     if [ "${OUTPUT}" == "200" ]; then
       echo -e "\e[0;32m DocumentServer is ready \e[0m"
+      local DS_READY
+      DS_READY='yes'
       break
     else
       sleep 10
     fi
   done
+  if [[ "${DS_READY}" != 'yes' ]]; then
+    err "\e[0;31m Something goes wrong documentserver does no started, check logs with command --> docker logs -f <ds-container-id> \e[0m"
+    exit 1
+  fi
 }
 
 complete_installation() {
