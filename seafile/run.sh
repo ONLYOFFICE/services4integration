46551be78e46acedfca22a43a739517187b91f19
kireevdmitry <octavarium78@gmail.com>
2023-03-15 13:44:54 +0000

    Fix jwt config

--- seafile/run.sh
+++ seafile/run.sh
@@ -6,4 +6,5 @@ VERIFY_ONLYOFFICE_CERTIFICATE = False
 ONLYOFFICE_APIJS_URL = 'http://${SEAFILE_SERVER_HOSTNAME}:3000/web-apps/apps/api/documents/api.js'
 ONLYOFFICE_FILE_EXTENSION = ('doc', 'docx', 'ppt', 'pptx', 'xls', 'xlsx', 'odt', 'fodt', 'odp', 'fodp', 'ods', 'fods')
 ONLYOFFICE_EDIT_FILE_EXTENSION = ('docx', 'pptx', 'xlsx')
-ONLYOFFICE_FORCE_SAVE = True" >> /opt/seafile/conf/seahub_settings.py
+ONLYOFFICE_FORCE_SAVE = True
+ONLYOFFICE_JWT_SECRET = '${SEAFILE_SERVER_HOSTNAME}'" >> /opt/seafile/conf/seahub_settings.py
