799a3c3bbaab49c50e58229283a04ff8b832fe6c
Artur Shakirov <64580664+xRTRx@users.noreply.github.com>
2023-04-26 06:35:31 +0000

    Edit readiness_check

--- discourse/install.sh
+++ discourse/install.sh
@@ -64,22 +64,23 @@ install_documentserver() {
 #   Status of the services
 #############################################################################################
 readiness_check() {
-   for i in {1..10}; do
+   for i in {1..20}; do
       DISCOURSE_STATUS_CODE=$(curl -sL -o /dev/null -w "%{http_code}" http://localhost:80)
-      DS_STATUS_CODE=$(curl -sL -o /dev/null -w "%{http_code}" http://localhost:3000)
+      DS_STATUS=$(curl -sL http://localhost:3000/healthcheck)
+
+      if [ "$DISCOURSE_STATUS_CODE" != "200" ] && [ "$DS_STATUS" != "true" ]; then
+         echo -e "\e[0;31m The services are not ready \e[0m"
+         return
+      elif [ "$DISCOURSE_STATUS_CODE" != "200" ]; then
+         echo -e "\e[0;31m The discourse service is not ready \e[0m"
+         return
+      elif [ "$DS_STATUS" != "true" ]; then
+         echo -e "\e[0;31m The Onlyoffice Document Server service is not ready \e[0m"
+         return
+      fi
+
       sleep 10
    done
-
-   if [ "$DISCOURSE_STATUS_CODE" != "200" ] && [ "$DS_STATUS_CODE" != "200" ]; then
-      echo -e "\e[0;31m The services are not ready \e[0m"
-      return
-   elif [ "$DISCOURSE_STATUS_CODE" != "200" ]; then
-      echo -e "\e[0;31m The discourse service is not ready \e[0m"
-      return
-   elif [ "$DS_STATUS_CODE" != "200" ]; then
-      echo -e "\e[0;31m The Onlyoffice Document Server service is not ready \e[0m"
-      return
-   fi
 
    echo -e "\e[0;32m The services are ready \e[0m"
 }
