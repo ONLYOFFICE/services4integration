7da77f8fe82a755fda1fdb3fd63f520a5af97ee1
kireevdmitry <octavarium78@gmail.com>
2024-02-29 12:31:10 +0000

    Translate comments

--- common/gen_password.sh
+++ common/gen_password.sh
@@ -1,10 +1,10 @@
 gen_password() {
-YMBOLS=""
+SYMBOLS=""
 for symbol in {A..Z} {a..z} {0..9}; do SYMBOLS=$SYMBOLS$symbol; done
 SYMBOLS=$SYMBOLS'!@#$%&*()?/\[]{}-+_=<>.,'
-: ${PWD_LENGTH:=16}  # длина пароля
-PASSWORD=""    # переменная для хранения пароля
-RANDOM=256     # инициализация генератора случайных чисел
+: ${PWD_LENGTH:=16}  # password length
+PASSWORD=""    # variable to store password
+RANDOM=256     # initializing the random number generator
 for i in `seq 1 $PWD_LENGTH`
 do
 PASSWORD=$PASSWORD${SYMBOLS:$(expr $RANDOM % ${#SYMBOLS}):1}
