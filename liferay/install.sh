a20c5ebfb0433cfb9ef18eef34dc02cfc8ebcba4
kireevdmitry <octavarium78@gmail.com>
2025-04-01 05:58:39 +0000

    Add the ability for moodle to work via https

--- liferay/install.sh
+++ liferay/install.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 #
 # Prepare a stand with Liferay with the dependent service Onlyoffice Document Server and add a connector
-
-SERVICE_TAG='7.4.0-ga1'
-CONNECTOR_URL='https://github.com/ONLYOFFICE/onlyoffice-liferay/releases/download/v2.0.0/onlyoffice.integration.web-2.0.0-CE7.4GA1.jar'
+APP="liferay"
+SERVICE_TAG='7.4.3.129-ga129'
+CONNECTOR_URL='https://github.com/ONLYOFFICE/onlyoffice-liferay/releases/download/v3.0.0/liferay-docs-3.0.0-CE-7.4.jar'
 CONNECTOR_NAME='onlyoffice-integration-web-liferay.jar'
 JWT_SECRET="mysecret"
+DS_TAG="latest"
+NGINX_CONF="nginx.conf"
+SCHEME='http'
+APP_ADDR=$(wget -q -O - ifconfig.me/ip)
 source /app/common/check_parameters.sh "${@}"
 source /app/common/error.sh
 source /app/common/jwt_configuration.sh
@@ -23,10 +27,42 @@ install_liferay() {
   source /app/common/install_dependencies.sh
   install_dependencies
   jwt_configuration
-  docker run -i -t -d --restart=always --name onlyoffice-document-server -p 3000:80 -e $JWT_ENV onlyoffice/documentserver
-  docker run -i -t -d --restart=always --name liferay -p 80:8080 liferay/portal:"${SERVICE_TAG}"
+  if [ "${DOMAIN_NAME}" ]; then
+    source /app/common/get_cert.sh
+    get_cert
+    APP_ADDR=${DOMAIN_NAME}
+    SCHEME='https'
+    NGINX_CONF="nginx_https.conf"
+    SSL_PROXY="yes"
+  fi
+  prepare_files
+  docker-compose -f /app/liferay/docker-compose.yml up -d
   echo OK > /opt/run
   echo -e "\e[0;32m Installation is complete \e[0m"
+}
+
+###################################################################################################
+# prepare global variables for docker-compose
+# Globals:
+#   JWT_ENV
+#   SERVICE_TAG
+#   DS_TAG
+#   NGINX_CONF
+#   SCHEME
+#   APP_ADDR
+# Arguments:
+#   None
+# Returns:
+#   None
+###################################################################################################
+prepare_files() {
+echo "JWT_ENV=${JWT_ENV}
+SERVICE_TAG=${SERVICE_TAG}
+DS_TAG=${DS_TAG}
+NGINX_CONF=${NGINX_CONF}
+SCHEME=${SCHEME}
+APP_ADDR=${APP_ADDR}
+" > /app/liferay/.env
 }
 
 ###################################################################################################
@@ -38,22 +74,44 @@ install_liferay() {
 # Returns:
 #   0, if the start is successful, non-zero on error
 ###################################################################################################
-check_launch_liferay() {
-  echo -e "\e[0;32m Waiting for the launch of Liferay \e[0m"
-  for ((i=1 ; i <= 100 ; i++)); do
-    echo "Getting the Liferay status: ${i}"
-    docker ps | grep liferay | grep -w healthy
+check_ready() {
+  local app_started
+  local healthcheck_ds
+  local ds_started
+
+  for i in {1..30}; do
+    curl -f -s ${SCHEME}://${APP_ADDR} > /dev/null
     if [[ "$?" -ne 0 ]]; then
-      sleep 5
+      echo -e "\e[0;32m Waiting for the launch of app \e[0m"
+        sleep 10
     else
-      echo -e "\e[0;32m Liferay works \e[0m"
-      local LIFERAY_READY
-      LIFERAY_READY='yes'
+      echo -e "\e[0;32m ${APP} works \e[0m"
+      app_started='true'
       break
     fi
   done
-  if [[ "${LIFERAY_READY}" != 'yes' ]]; then
-    err "\e[0;31m I didn't wait for the launch of Liferay. Check the container logs using the command: sudo docker logs -f Liferay \e[0m"
+
+  if [[ "${app_started}" != 'true' ]]; then
+    err "\e[0;31m I didn't wait for the launch of app. \e[0m"
+    exit 1
+  fi
+
+  for i in {1..30}; do
+    healthcheck_ds="$(curl -f -s ${SCHEME}://${APP_ADDR}/ds-vpath/healthcheck)"
+    if [[ "${healthcheck_ds}" != 'true' ]]; then
+      echo -e "\e[0;32m Waiting for the launch of document-server \e[0m"
+        sleep 10
+    else
+      echo -e "\e[0;32m document-server is running \e[0m"
+      ds_started='true'
+      break
+    fi
+  done
+
+  if [[ "${ds_started}" != 'true' ]]; then
+    err "\e[0;31m Document server did not start. \
+Check the container logs using the command: \
+sudo docker logs -f onlyoffice-document-server. \e[0m"
     exit 1
   fi
 }
@@ -74,43 +132,18 @@ prepare_connector() {
   docker cp /connectors/${CONNECTOR_NAME} liferay:/opt/liferay/deploy/
 }
 
-###################################################################################################
-# Check whether the connector is loaded into the container
-# Arguments:
-#   None
-# Outputs:
-#   Writes the verification status message to stdout
-# Returns:
-#   0, if the check was successful, non-zero on error
-###################################################################################################
-check_connector_in_container() {
-  for ((i=1 ; i <= 100 ; i++)); do
-    echo "Checking whether the connector is loaded into the container: ${i}"
-    docker logs liferay | grep -i "STARTED onlyoffice.integration.web"
-    if [[ "$?" -ne 0 ]]; then
-      sleep 5
-    else
-      echo -e "\e[0;32m The connector was successfully added to liferay. Ready to go \e[0m"
-      local CONNECTOR_EXISTS
-      CONNECTOR_EXISTS='yes'
-      break
-    fi
-  done
-  if [[ "${CONNECTOR_EXISTS}" != 'yes' ]]; then
-    err "\e[0;31m An error occurred when adding a connector to liferay. Check the container logs using the command: sudo docker logs -f liferay \e[0m"
-    exit 1
-  fi
-}
-
 complete_installation(){
+  echo -e "\e[0;32m The script is finished \e[0m"
+  echo -e "\e[0;32m Then you can go to the ${APP} web interface at: \
+  ${SCHEME}://${APP_ADDR} and check the connector operation. \e[0m"
+  echo -e "\e[0;32m User: user  Password: ${PASSWORD} \e[0m"
   echo -e "\e[0;32m The script is finished \e[0m"
 }
 
 main() {
   install_liferay
-  check_launch_liferay
+  check_ready
   prepare_connector
-  check_connector_in_container
   complete_installation
 }
 
