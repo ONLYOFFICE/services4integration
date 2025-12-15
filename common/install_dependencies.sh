f0bdc16856a43fbb4fbd2515894f33dcad3d8756
Dmitry Kireev <dmitriy.kireev@onlyoffice.com>
2025-12-15 14:21:17 +0000

    Change docker-compose version (#10)
    Co-authored-by: kireevdmitry <octavarium78@gmail.com>
    Reviewed-on: https://git.onlyoffice.com/ONLYOFFICE/services4integration/pulls/10

--- common/install_dependencies.sh
+++ common/install_dependencies.sh
@@ -6,10 +6,10 @@
 install_dependencies() {
   apt-get update && apt-get install -y apt-transport-https curl wget ca-certificates software-properties-common gnupg2 ntp net-tools unzip
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
   apt update
   apt install -y docker-ce docker-ce-cli
   systemctl start docker
-  curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  curl -L "https://github.com/docker/compose/releases/download/v2.40.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 }
